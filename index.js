72410ff6677723264af8002911c9f13ce1c2964e
OpenShift guest <zhongdeliu@googlemail.com>
2013-10-14 09:23:43 -0400

    added more objects

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('Server index');
 });
-
+app.get('/board/:id', function(req, res) {
+    var board = {
+        id:  Number(req.params.id)
+    };
+    return board;
+});
 
 app.get('/brands', function(req, res) {
   var output = [];
   var item = {};
   for (var i = 0; i < 10; i++) {
     item = {
-      id: "0" + Faker.Helpers.randomNumber(999999),
+      id: "0-" + Faker.Helpers.randomNumber(999999),
       name: Faker.Company.companyName(),
-      image: 'placeholder_brand.jpg',
+      image: 'placeholder/placeholder_brand.png',
       series: []
     };
     for (var j = 0; j < Faker.Helpers.randomNumber(100) % 10 + 5; j++) {
         item.series.push({
-            id: "1" + Faker.Helpers.randomNumber(999999),
+            id: "1-" + Faker.Helpers.randomNumber(999999),
             name: Faker.Name.lastName(),
-            image: 'placeholder_series.jpg'
+            image: 'placeholder/placeholder_series.png'
         });
     }
     output.push(item);
@@ -40,11 +45,19 @@ app.get('/brands', function(req, res) {
 app.get('/categories', function(req, res) {
   var output = [];
   for (var i = 0; i < Faker.Helpers.randomNumber(100) % 5 + 5; i++) {
-    output.push({
-      id: "5" + Faker.Helpers.randomNumber(999999),
+    var item = {
+      id: "5-" + Faker.Helpers.randomNumber(999999),
       name: Faker.Name.lastName(),
-      image: 'http://placehold.it/100x100'
-    });
+      image: 'placeholder/placeholder_category.png',
+      subject: []
+    };
+    for (var j = 0; j < Faker.Helpers.randomNumber(100) % 10 + 5; j++) {
+        item.subject.push({
+            id: "6-" + Faker.Helpers.randomNumber(999999),
+            name: Faker.Name.lastName(),
+        });
+    }
+    output.push(item);    
   }
   var body = JSON.stringify(output);
 
@@ -56,7 +69,7 @@ app.get('/categories', function(req, res) {
   res.end(body);
 });
 
-app.get('/items/:id?/:brand?/:category?', function(req, res) {
+app.get('/products/:id?/:brand?/:category?', function(req, res) {
   var category = Number(req.params.category);
   var brand = Number(req.params.brand);
   var id = Number(req.params.id);
@@ -65,13 +78,13 @@ app.get('/items/:id?/:brand?/:category?', function(req, res) {
   var output = [];
   for (var i = 0; i < Faker.Helpers.randomNumber(100) + 20; i++) {
       item = {
-      id: "9" + Faker.Helpers.randomNumber(999999),
+      id: "9-" + Faker.Helpers.randomNumber(999999),
       category_id: "5" + Faker.Helpers.randomNumber(999999),
       brand_id: "0" + Faker.Helpers.randomNumber(999999),
       name: Faker.Name.lastName(),
       description_short: Faker.Lorem.sentence(),
       description_long: Faker.Lorem.sentences(),
-      image: 'http://placehold.it/100x100'
+      image: 'placeholder/placeholder_product.png'
     };
     if (id && !isNaN(id)) {
         item.id = id;
@@ -84,6 +97,28 @@ app.get('/items/:id?/:brand?/:category?', function(req, res) {
     }
     output.push(item);
   }
+  var body = JSON.stringify(output);
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  });
+
+  res.end(body);
+});
+
+app.get('/backgrounds', function(req, res) {
+  var output = [];
+  var item = {};
+  for (var i = 0; i < 10; i++) {
+    item = {
+      id: "3-" + Faker.Helpers.randomNumber(999999),
+      name: Faker.Company.companyName(),
+      image: 'bg.jpg'
+    };
+    output.push(item);
+  }
+  
   var body = JSON.stringify(output);
 
   res.writeHead(200, {
